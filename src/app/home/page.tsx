afe478a902f072adf8b8d8ca5fee927091aa7439
Sumanth2418 <sumanthreddynerella@gmail.com>
2026-06-28 18:45:31 +0100

    first

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+      <p>Sumanth Home page</p>
+    </div>
+  );
+}
