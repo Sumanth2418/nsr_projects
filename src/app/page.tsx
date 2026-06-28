850794c5e99e1e543f8ce83bfb752158fce439d8
Sumanth2418 <sumanthreddynerella@gmail.com>
2026-06-28 18:56:45 +0100

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p>Sumanth first site</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab illum voluptas sapiente nisi cum nemo sequi, dolores distinctio atque perspiciatis assumenda est iusto sit ipsum a numquam laudantium officiis repellat.</p>
     </div>
   );
 }
