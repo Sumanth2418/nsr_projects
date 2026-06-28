c9684919c1a5c760b345ea719e3c2671908489ec
Sumanth2418 <sumanthreddynerella@gmail.com>
2026-06-28 22:24:48 +0100

    second-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 bg-gray-100">
+    <div className="flex flex-col flex-1items-center justify-center gap-4 bg-gray-100">
       <div className="h-24 w-24 bg-red-200 text-white flex items-center justify-center">
         1
       </div>
